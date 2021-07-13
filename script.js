5a30f3768600c8f515d7a2e213e630653b67c116
Madu Rubini <mariaed.rubini@gmail.com>
2021-07-13 17:38:12 -0300

    Logica Finalizada

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function oneTurn() {
     let diceSum = [];
     for(let i = 0; i < 2; i++){
         diceSum.push(rollOfDice());
-        console.log(diceSum)
     }   
     return diceSum.reduce((total,currentElement) => total + currentElement)
 } 
+
+const allTurns = ()  => {
+    let resultado = [];
+    const count = [0,0,0,0,0,0,0,0,0,0,0]
+     for(let j = 0; j<=1000; j++){
+         resultado = oneTurn()
+         count[resultado-2]++//count[resultado-2] = count[resultado-2] +1 
+       
+    }
+    
+    return count;
+}
+  
+//jogar os dados - 4
+//guardar o resultado dos dados 
+//encontrar o indice referente ao resultado count[resultado-2] 
+//somar +1 no valor do indice
+
+
+
+
