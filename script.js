239b00932d6ca9814a78c3c348e4c7897f768451
Madu Rubini <mariaed.rubini@gmail.com>
2021-07-13 15:17:25 -0300

    Inserindo as funções rollOfDice() responsável por gerar um número aleatório e a função oneTurn, responsável por uma rodada
    git oi
    Inserindo as funções rollOfDice() responsável por gerar um número aleatório e a função oneTurn, responsável por uma rodada

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+//Função que gera um número aleatório de 1-6
 
+const rollOfDice = () => Math.floor(Math.random() * 6 + 1 );
+
+function oneTurn() {
+    let diceSum = [];
+    for(let i = 0; i < 2; i++){
+        diceSum.push(rollOfDice());
+        console.log(diceSum)
+    }   
+    return diceSum.reduce((total,currentElement) => total + currentElement)
+} 
